7a98890e5f209391e7d6cf39a4340fe03f486f80
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-07 17:23:13 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,4 +3,10 @@ module Players
    def move(board)
      position = random(board)
   end
+  def random(board)
+        position = rand(1..9).to_s
+        board.valid_move?(position) ? position : random(board)
+      end
+    end   
+
 end
